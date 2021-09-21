c9fc3f599dc98fa9aa78256f512f09a406fcb2a2
alesitom <alemendoza1990@gmail.com>
2021-09-20 23:34:06 -0300

    update en booklist.js, bookcontext.js y index.css para que se muestre como tabla.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -10,9 +10,36 @@ const BookList = () => {
     return (
         <div className="book-list" style={themeStyles}>
             <h2>Lista de Libros</h2>
-            <ul>
+            {/* <ul>
             {books.map(book => <li key={book.id}>{book.title}</li>)}
-            </ul>
+            </ul> */}
+
+            {/* <ul>
+            {books.map(book => <li key={book.id}>{book.title} - {book.author}</li>)}
+            </ul> */}
+            <table class="table">
+  <thead class="table table-striped">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Libro</th>
+      <th scope="col">Autor</th>
+    </tr>
+  </thead>
+  <tbody>
+  {books.map(book =>
+    <tr>
+      <th scope="row" key={book.id}>{book.id}</th>
+      <td>{book.title}</td>
+      <td>{book.author}</td>
+    </tr>
+  )}
+  </tbody>
+</table>
+
+
+
+
+
         </div>
     )
 }
--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -4,10 +4,10 @@ export const BookContext = createContext()
 
 const BookContextProvider = (props) => {
     const [books, setBooks] = useState([
-        {title: 'Harry Potter', id: 1},
-        {title: 'El Principito', id: 2},
-        {title: 'Cuentos Para Monstruos', id: 3},
-        {title: 'Código Da Vinci', id: 4},
+        {title: 'Harry Potter', id: 1, author:'JK Rowling'},
+        {title: 'El Principito', id: 2, author:'Antoine de Saint-Exupéry'},
+        {title: 'Cuentos Para Monstruos', id: 3, author:'Santiago González Pedraza'},
+        {title: 'Código Da Vinci', id: 4, author:'Dan Brown'},
     ])
 
     return (
